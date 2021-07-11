fd1a13ba03c402dbeb0b398fe24bd5187e8efde9
Siddhartha Dutta <siddharthadutta@protonmail.com>
2021-07-11 15:09:23 +0530

    Lab2 Implementation

--- Lab2/Code/src/AccManager.java
+++ Lab2/Code/src/AccManager.java
@@ -1,8 +1,15 @@
 public class AccManager {
 	public void fillAccountData(Account a1) {
-	
-	//Complete the statements
-
+		System.out.print("Enter account number : ");
+		String accNo = Console.readLine();
+		System.out.print("Enter name : ");
+		String name = Console.readLine();
+		System.out.print("Enter balance amount : ");
+		double balance = Console.readDouble();
+		
+		a1.setAccNo(accNo);
+		a1.setName(name);
+		a1.setBalance(balance);
 	}
 	public void displayAccountData(Account a1) {
 		System.out.println("AccNo : " + a1.getAccNo());
