cec19278a87fdd1a0d5e43fabf0ea0b69ff309d9
Siddhartha Dutta <siddharthadutta@protonmail.com>
2021-07-11 15:53:14 +0530

    Lab7 Implementation

--- Lab7/Code/src/ResultFinder.java
+++ Lab7/Code/src/ResultFinder.java
@@ -2,24 +2,46 @@ public class ResultFinder {
 	private int m1;
 	private int m2;
 	private int m3;
-	
-	// Create setter and getter methods 
+
+	public int getM1() {
+		return m1;
+	}
+
+	public int getM2() {
+		return m2;
+	}
+
+	public int getM3() {
+		return m3;
+	}
+
+	public void setM1(int m1) {
+		this.m1 = m1;
+	}
+
+	public void setM2(int m2) {
+		this.m2 = m2;
+	}
+
+	public void setM3(int m3) {
+		this.m3 = m3;
+	}
 
 	public void displayMarks() {
-
-
-	
+		System.out.println("m1=" + m1 + ", m2=" + m2 + ", m3=" + m3);
 	}
+
 	public int getTotal() {
-//		complete the logic
+		return m1 + m2 + m3;
 	}
+
 	public double getAverage() {
-//		complete the logic
+		return getTotal() / 3;
 	}
-	
+
 	public String getResult() {
-
-//		complete the logic
-
+		if (m1 >= 35 && m2 >= 35 && m3 >= 35)
+			return "Pass";
+		return "Fail";
 	}
 }
--- Lab7/Code/src/TestMain.java
+++ Lab7/Code/src/TestMain.java
@@ -1,21 +1,19 @@
 public class TestMain {
 	public static void main(String[] args) {
-		if(args.length != 3) {
+		if (args.length != 3) {
 			System.out.println("Please enter 3 marks!");
 			System.exit(0);
 		}
-		int m1 = 0;
-		int m2 = 0;
-		int m3 = 0;
-		
+		int m1 = Integer.parseInt(args[0]);
+		int m2 = Integer.parseInt(args[1]);
+		int m3 = Integer.parseInt(args[2]);
 
-		
 		ResultFinder finder = new ResultFinder();
-		
-
-		
+		finder.setM1(m1);
+		finder.setM2(m2);
+		finder.setM3(m3);
 		finder.displayMarks();
-		
+
 		System.out.println("Total : " + finder.getTotal());
 		System.out.println("Average : " + finder.getAverage());
 		System.out.println("Result : " + finder.getResult());
