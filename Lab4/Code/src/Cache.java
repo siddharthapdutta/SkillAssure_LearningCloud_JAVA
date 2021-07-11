892244a721926557f750e6fb387e14e21022480c
Siddhartha Dutta <siddharthadutta@protonmail.com>
2021-07-11 15:33:32 +0530

    Lab4 Implementation

--- Lab4/Code/src/Cache.java
+++ Lab4/Code/src/Cache.java
@@ -1,7 +1,10 @@
 public class Cache {
 	private static int MAX_CAPACITY = 0;
 	
-	// Implement static block
+	static {
+		System.out.print("Enter Max Capacity: ");
+		MAX_CAPACITY = Console.readInt();
+	}
 
 	static int getMaxCapacity() {
 		System.out.println("Returning MAX_CAPACITY");
